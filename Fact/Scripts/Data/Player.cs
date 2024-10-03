93ef849536c89092f536b612c61fe52a055289b4
wlg-shinya <shinya.watanabe@welovegamesinc.com>
2024-10-03 15:38:01 +0900

    選手データを追加

---
+++ Fact/Scripts/Data/Player.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using Wlg.FigureSkate.Core.Data;
+
+namespace Wlg.FigureSkate.Fact.Data
+{
+    // 選手
+    [Serializable]
+    public class Player
+    {
+        // 名前
+        public string name;
+        // スコア上の名前
+        public string nameOnScore;
+        // 生年月日
+        public YearMonthDay birthday;
+        // 性別
+        public string sexId;
+        // クラス
+        public string classId;
+        // 国籍
+        public string nation;
+        // 実行可能な構成要素
+        public List<string> elementIdList = new();
+        // プログラム構成
+        [Serializable]
+        public class ProgramComponents
+        {
+            public ProgramComponent[] components;
+        }
+        public ProgramComponents[] programComponentsList;
+    }
+}
