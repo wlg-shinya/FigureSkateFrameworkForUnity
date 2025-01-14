8225dcbcc3ffb99759f2f8dfe22ee1569c13ee87
wlg-shinya <shinya.watanabe@welovegamesinc.com>
2025-01-14 15:44:00 +0900

    文字の全角・半角変換クラスの追加

---
+++ Fact/Scripts/StringWidthConverter.cs
@@ -0,0 +1,32 @@
+namespace Wlg.FigureSkate.Fact
+{
+    // https://koma2961.hatenablog.com/entry/2016/02/24/051156
+    public class StringWidthConverter
+    {
+        const int ConvertionConstant = 65248;
+
+        static public string ConvertToFullWidth(string halfWidthStr)
+        {
+            string fullWidthStr = null;
+
+            for (int i = 0; i < halfWidthStr.Length; i++)
+            {
+                fullWidthStr += (char)(halfWidthStr[i] + ConvertionConstant);
+            }
+
+            return fullWidthStr;
+        }
+
+        static public string ConvertToHalfWidth(string fullWidthStr)
+        {
+            string halfWidthStr = null;
+
+            for (int i = 0; i < fullWidthStr.Length; i++)
+            {
+                halfWidthStr += (char)(fullWidthStr[i] - ConvertionConstant);
+            }
+
+            return halfWidthStr;
+        }
+    }
+}
