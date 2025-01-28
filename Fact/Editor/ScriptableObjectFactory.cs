d38c282963d525bbf30f65f22f628cc72b6632b4
wlg-shinya <shinya.watanabe@welovegamesinc.com>
2025-01-28 11:05:51 +0900

    todo memo

--- Fact/Editor/ScriptableObjectFactory.cs
+++ Fact/Editor/ScriptableObjectFactory.cs
@@ -9,6 +9,7 @@ using Wlg.FigureSkate.Core.ScriptableObjects;
 
 namespace Wlg.FigureSkate.Fact.Editor
 {
+    // TODO:Objects以下のファイルのAddressable設定をフレームワーク側で用意する
     public class ScriptableObjectFactory : AssetPostprocessor
     {
         static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
