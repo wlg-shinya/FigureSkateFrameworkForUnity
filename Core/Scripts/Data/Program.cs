cf31186c0e519f47882c652a31ec3c4e2558a363
wlg-shinya <shinya.watanabe@welovegamesinc.com>
2025-02-14 18:38:53 +0900

    Programが性別とクラスを省いた名称(shortName)を保持するように

--- Core/Scripts/Data/Program.cs
+++ Core/Scripts/Data/Program.cs
@@ -2,14 +2,16 @@ using System;
 
 namespace Wlg.FigureSkate.Core.Data
 {
-    // プログラム（ショートプログラム、フリースケーティングなど）
+    // プログラム（シニア男子ショートプログラム、ジュニア女子フリースケーティングなど）
     [Serializable]
     public class Program
     {
         // 一意の記号
         public string id;
-        // 名称
+        // 正式名称
         public string name;
+        // 性別とクラスを省いた名称
+        public string shortName;
         // 後半ジャンプボーナス数
         public int lastJumpSpecialFactorCount;
         // 演技構成点倍率
