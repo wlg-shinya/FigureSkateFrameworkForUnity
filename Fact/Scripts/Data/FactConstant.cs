391963e2d295f70d2d7de6b1d7ec957787127ed6
wlg-shinya <shinya.watanabe@welovegamesinc.com>
2025-04-18 19:08:36 +0900

    定数に選手一人のプログラム構成の最大数を追加

--- Fact/Scripts/Data/FactConstant.cs
+++ Fact/Scripts/Data/FactConstant.cs
@@ -3,11 +3,14 @@ namespace Wlg.FigureSkate.Fact
     public static class FactConstant
     {
         // このフレームワークが対応しているシーズン
-        public static string[] SEASONS = new[]
+        public static readonly string[] SEASONS = new[]
         {
             "2022-23",
             "2023-24",
             "2024-25",
         };
+
+        // 選手一人のプログラム構成の最大数
+        public static readonly int PLAYER_PROGRAM_COMPONENTS_MAX_COUNT = 2;
     }
 }
