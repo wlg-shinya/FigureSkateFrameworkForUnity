c08028c5d3be36f06cb10b03d464b9a2f4d110e4
wlg-shinya <shinya.watanabe@welovegamesinc.com>
2025-07-31 16:56:55 +0900

    TES最大項目数の定数を追加

--- Core/Scripts/Data/CoreConstant.cs
+++ Core/Scripts/Data/CoreConstant.cs
@@ -10,6 +10,8 @@ namespace Wlg.FigureSkate.Core
         public const int GOE_MIN_VALUE = -5;
         // GOE1あたりのスコア倍率
         public const float GOE_SCORE_MAGNIFICATION = 0.1f;
+        // TES最大項目数
+        public const int TES_MAX_COUNT = 12;
         // PCS項目数
         public const int PCS_COUNT = 3;
         // 審判の人数
